46c877bb70f1a1535b68b8e63e4834e4212aa6c6
sayalis <sayalis@10.10.10.172>
2026-04-06 16:26:17 +0530

    jwt token implementation

--- login-signup/src/main/java/com/microblog/login_signup/Model/ResponseEntity/LoginResponse.java
+++ login-signup/src/main/java/com/microblog/login_signup/Model/ResponseEntity/LoginResponse.java
@@ -8,15 +8,30 @@ import lombok.Data;
 public class LoginResponse {
 
 	public String msg;
+	public String token;
 	
 	public String name;
 	public String username;
 	public String email;
 	public String password;
+	
+	
 
 	public LoginResponse(String msg) {
 		super();
 		this.msg = msg;
 	}
+
+	public LoginResponse(String msg, String token) {
+		super();
+		this.msg = msg;
+		this.token = token;
+	}
+
+	public LoginResponse() {
+		
+	}
+
+	
 	
 }
